e52cf078eec830ccf21d45aaaede929e7cb68cf2
pitwan1 <isanchezcalle1995@gmail.com>
2024-03-17 23:37:26 +0100

    Primer commit

---
+++ Holamundo/src/HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+
+    public static void main(String[] args){
+        String nombre = "Ivan";
+        System.out.println("Hola, mi nombre es " + nombre);
+
+
+    }
+    
+}
